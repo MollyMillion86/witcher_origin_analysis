4d770a9b8c98466d9c57551ce8a8d684a6974390
MollyMillion86 <molly.million.1984@gmail.com>
2019-06-13 10:33:54 +0200

    home: delay fadeIn OK

--- js/witcher.js
+++ js/witcher.js
@@ -5,12 +5,13 @@
 $(document).ready(function() {
 	
 
-	// menu
-	
-	
-	$("#home-title").fadeIn(3000, function() {
-		$("#home-menu").fadeIn(1000);	
+	// mostra nell'ordine MEDAGLIONE, TITOLO e MENU
+	$("#medallion-img").delay(1000).fadeIn(3000, function() {
+		$("#home-title").fadeIn(1000, function() {
+			$("#home-menu").fadeIn(1000);	
+		});
 	});
+		
 	
 	
 });
