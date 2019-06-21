c61fd674b4290673140e577bba75a3347e4351e7
MollyMillion86 <molly.million.1984@gmail.com>
2019-06-21 11:10:58 +0200

    border bottom and title fonts

--- js/witcher.js
+++ js/witcher.js
@@ -1,19 +1,28 @@
 
 
 
+function fadeTitle() {
+	$("#home-title").fadeIn(1000, function() { fadeAndBorderMenu() });
+}
+
+function fadeAndBorderMenu() {
+	$("#main-menu").fadeIn(1000 , function() {
+		$("#menu-list > li").css("border-bottom", "1px solid red");
+	});
+}
+
+
+
+
 
 $(document).ready(function() {
 	
 
 	// mostra nell'ordine MEDAGLIONE, TITOLO e MENU
-	$("#medallion-img").delay(1000).fadeIn(3000, function() {
-		$("#home-title").fadeIn(1000, function() {
-			$("#home-menu").fadeIn(1000);	
-		});
-	});
+	$("#medallion-img").delay(1000).fadeIn(3000, function() { fadeTitle() });
 	
 	
-	// 
+	
 	
 	
 });
