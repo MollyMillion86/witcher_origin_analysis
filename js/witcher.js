1dbbf5aa22e319434b7ae1a75abbdf3e451df31d
Molly Million <30889409+MollyMillion86@users.noreply.github.com>
2022-08-25 17:26:52 +0200

    ripristino animazioni titolo, medaglione e menu in home

--- js/witcher.js
+++ js/witcher.js
@@ -1,31 +1,43 @@
 
 
-/* 
+/**
+* fadeTitle
+* 
+* Mostra titolo e subito dopo menu e bordo menu
+*/
 function fadeTitle() {
-	$("#home-title").fadeIn(1000, function() { fadeAndBorderMenu() });
+	$("#home-title").fadeIn(1000, function() { 
+		fadeAndBorderMenu();
+	});
 }
 
+/**
+* fadeTitle
+* 
+* Mostra menu e bordo menu
+*/
 function fadeAndBorderMenu() {
-	$("#main-menu").fadeIn(1000 , function() {
+	$("#main-menu").fadeIn(2000, function() {
 		$("#menu-list > li").css("border-bottom", "1px solid red");
 	});
 }
- */
+
 
 
 
 
 $(document).ready(function() {
 	
-
+	
 	// mostra nell'ordine MEDAGLIONE, TITOLO e MENU
-	// $("#medallion-img").delay(1000).fadeIn(3000, function() { fadeTitle() });
-	
-	
-	/* 
-	$("#home-title").fadeIn(1000, function() {
-		$("#home-menu").fadeIn(1000);	
+	$("#medallion-img").delay(1000).fadeIn(3000, function() { 
+		fadeTitle();
 	});
-	 */
+	
+	
+	
+	$("#home-title").fadeIn(2000, function() {
+		$("#menu-list").fadeIn(1000);
+	});
 	
 });
